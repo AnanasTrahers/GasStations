82b4a5d01273b2f5403243150f048ee0c5377d36
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-05-27 19:32:14 +0300

    [add] functions to fetch fuel prices and calculate total price

--- src/config.py
+++ src/config.py
@@ -7,9 +7,12 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+
     MAPBOX_API_KEY: str
     MAPBOX_BASE_URL: str
     MAPBOX_DIRECTIONS_ENDPOINT: str
+
+    FUEL_PRICE_SAFE_DAYS: int
 
     model_config = SettingsConfigDict(
         env_file=ROOT_DIR / ".env",
--- src/service.py
+++ src/service.py
@@ -1,13 +1,15 @@
 from collections.abc import Sequence
+from datetime import datetime, timezone, timedelta
 
-from shapely.geometry import LineString
+from shapely.geometry import LineString, Point
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import joinedload
 from sqlalchemy.sql.expression import select, func, cast
 from geoalchemy2 import Geography
 from geoalchemy2.shape import to_shape
 
-from src.models import GasStation
+from src.config import settings
+from src.models import GasStation, FuelPrice
 from src.schemas import StationDTO
 
 
@@ -41,7 +43,7 @@ def map_stations_to_dto(
 ) -> list[StationDTO]:
     result = []
     for station in gas_stations:
-        point = to_shape(station.geom)
+        point = cast(Point, to_shape(station.geom))
 
         result.append(
             StationDTO(
@@ -53,3 +55,56 @@ def map_stations_to_dto(
         )
 
     return result
+
+
+async def fetch_fuel_prices_per_liter(
+        gas_stations: list[StationDTO],
+        fuel_type: str,
+        session: AsyncSession
+) -> dict[int, float]:
+    network_ids = {station["network_id"] for station in gas_stations}
+    safe_date = (datetime.now(timezone.utc)
+                 - timedelta(days=settings.FUEL_PRICE_SAFE_DAYS))
+
+    stmt = (
+        select(FuelPrice.network_id, FuelPrice.price)
+        .where(
+            FuelPrice.fuel_type == fuel_type,
+            FuelPrice.network_id.in_(network_ids),
+            FuelPrice.created_at >= safe_date
+        )
+        .distinct(FuelPrice.network_id)
+        .order_by(
+            FuelPrice.network_id,
+            FuelPrice.created_at.desc()
+        )
+    )
+
+    result = await session.execute(stmt)
+
+    return {row.network_id: row.price for row in result.all()}
+
+
+def filter_gas_stations(
+        gas_stations: list[StationDTO],
+        prices_map: dict[int, float],
+) -> list[StationDTO]:
+    filtered_stations = []
+
+    for station in gas_stations:
+        fuel_price = prices_map.get(station["network_id"])
+        if fuel_price is not None:
+            station["price_per_liter"] = fuel_price
+            filtered_stations.append(station)
+
+    return filtered_stations
+
+
+def calculate_fuel_prices(
+        gas_stations: list[StationDTO],
+        volume: int
+) -> list[StationDTO]:
+    for station in gas_stations:
+        station["fuel_price"] = station["price_per_liter"] * volume
+
+    return gas_stations
