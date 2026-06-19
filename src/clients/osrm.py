665cf527f0e414924330ad6d87d76ecc1d174e1d
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-06-19 20:11:21 +0300

    [add] OSRMClient class

---
+++ src/clients/osrm.py
@@ -0,0 +1,24 @@
+import httpx
+
+from src.clients.base import BaseRoutingClient
+from src.config import settings
+from src.schemas import Coords, MatrixDirection
+
+
+class OSRMClient(BaseRoutingClient):
+    def __init__(self, client: httpx.AsyncClient):
+        super().__init__(client)
+        self.table_endpoint = settings.OSRM_TABLE_ENDPOINT
+
+    async def get_table(
+            self,
+            anchor: Coords,
+            coordinates: list[Coords],
+            direction: MatrixDirection
+    ) -> dict:
+        return await self._call_matrix(
+            endpoint=self.table_endpoint,
+            anchor=anchor,
+            coordinates=coordinates,
+            direction=direction
+        )
