b18c71e2341a43681f41de5aee54c8a94e89f40b
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-06-19 20:10:57 +0300

    [refactor] MapboxClient class to inherit from BaseRoutingClient

--- src/clients/mapbox.py
+++ src/clients/mapbox.py
@@ -2,29 +2,18 @@ from dataclasses import asdict
 
 import httpx
 
+from src.clients.base import BaseRoutingClient
 from src.config import settings
-from src.schemas import Coords, DirectionsParams
+from src.schemas import Coords, DirectionsParams, MatrixDirection
 
 
-class MapboxClient:
+class MapboxClient(BaseRoutingClient):
     def __init__(self, client: httpx.AsyncClient):
-        self.client = client
+        super().__init__(client)
         self.api_key = settings.MAPBOX_API_KEY
         self.directions_endpoint = settings.MAPBOX_DIRECTIONS_ENDPOINT
         self.matrix_endpoint = settings.MAPBOX_MATRIX_ENDPOINT
         self.isochrone_endpoint = settings.MAPBOX_ISOCHRONE_ENDPOINT
-
-    @staticmethod
-    def _build_coordinates_string(
-            coordinates: list[Coords]
-    ) -> str:
-        return ";".join(f"{coord.lng},{coord.lat}" for coord in coordinates)
-
-    @staticmethod
-    def _get_destination_or_source_indices(coordinates: str) -> str:
-        count = coordinates.count(";")
-
-        return ";".join(str(i) for i in range(1, count + 1))
 
     @staticmethod
     def _build_approaches_string(n: int) -> str:
@@ -44,54 +33,20 @@ class MapboxClient:
         params["access_token"] = self.api_key
         params["approaches"] = self._build_approaches_string(len(coords))
 
-        r = await self.client.get(endpoint, params=params)
-        r.raise_for_status()
-
-        return r.json()
+        return await self._execute_get(endpoint, params)
 
     async def get_forward_matrix(
             self,
             start: Coords,
             coordinates: list[Coords]
     ) -> dict:
-        coordinates_str = self._build_coordinates_string([start] + coordinates)
-        endpoint = self.matrix_endpoint + coordinates_str
-
-        destination_indices = self._get_destination_or_source_indices(coordinates_str)
-
-        params = {
-            "access_token": self.api_key,
-            "annotations": "duration,distance",
-            "sources": 0,
-            "destinations": destination_indices,
-        }
-
-        r = await self.client.get(endpoint, params=params)
-        r.raise_for_status()
-
-        return r.json()
-
-    async def get_backward_matrix(
-            self,
-            end: Coords,
-            coordinates: list[Coords]
-    ) -> dict:
-        coordinates_str = self._build_coordinates_string([end] + coordinates)
-        endpoint = self.matrix_endpoint + coordinates_str
-
-        source_indices = self._get_destination_or_source_indices(coordinates_str)
-
-        params = {
-            "access_token": self.api_key,
-            "annotations": "duration,distance",
-            "sources": source_indices,
-            "destinations": 0
-        }
-
-        r = await self.client.get(endpoint, params=params)
-        r.raise_for_status()
-
-        return r.json()
+        return await self._call_matrix(
+            endpoint=self.matrix_endpoint,
+            anchor=start,
+            coordinates=coordinates,
+            direction=MatrixDirection.FORWARD,
+            additional_params={"access_token": self.api_key}
+        )
 
     async def get_isochrones(self, point: Coords, radiuses: list[int]) -> dict:
         coordinates_str = self._build_coordinates_string([point])
@@ -104,7 +59,4 @@ class MapboxClient:
             "generalize": 50,
         }
 
-        r = await self.client.get(endpoint, params=params)
-        r.raise_for_status()
-
-        return r.json()
+        return await self._execute_get(endpoint, params)
