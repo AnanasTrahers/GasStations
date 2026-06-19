8a22e13037db7c78c43ced97b3c7e72e20f1c6a8
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-06-19 17:32:35 +0300

    [update] StationDTO attributes

--- src/schemas.py
+++ src/schemas.py
@@ -2,7 +2,6 @@ from typing import TypedDict, NotRequired
 from collections import namedtuple
 from dataclasses import dataclass
 
-from src.config import settings
 
 Coords = namedtuple("Coords", ["lng", "lat"])
 
@@ -12,35 +11,36 @@ class StationDTO(TypedDict):
     coordinates: Coords
     network_id: int
     network_name: str
+    fraction: float
 
+    segment_id: NotRequired[int]
     price_per_liter: NotRequired[float]
+    total_distance_m: NotRequired[float]
+    total_duration_s: NotRequired[float]
+    distance_difference_m: NotRequired[float]
+    duration_difference_s: NotRequired[float]
     fuel_price: NotRequired[float]
-    distance: NotRequired[float]
-    duration: NotRequired[float]
 
 
 @dataclass
 class DirectionsParams:
     geometries: str = "geojson"
+    overview: str = "full"
     annotations: str = None
-    overview: str = None
     steps: str = None
     banner_instructions: str = None
     language: str = None
     voice_instructions: str = None
     voice_units: str = None
-    approaches: str = None
 
 
     @classmethod
     def setup_full_request(cls):
         return cls(
             annotations="distance,duration,speed",
-            overview="full",
             steps="true",
             banner_instructions="true",
             language="uk",
             voice_instructions="true",
-            voice_units="metric",
-            approaches="curb",
+            voice_units="metric"
         )
