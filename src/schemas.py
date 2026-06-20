db1c9dff598c0c90919dbd7d8660954902bbd59a
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-06-20 23:05:19 +0300

    [refactor] replace DTO with pydantic schema

--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,8 @@
 from enum import Enum
-from typing import TypedDict, NotRequired
 from collections import namedtuple
 from dataclasses import dataclass
+from pydantic import BaseModel, ConfigDict
+from math import floor
 
 
 Coords = namedtuple("Coords", ["lng", "lat"])
@@ -11,20 +12,58 @@ class MatrixDirection(str, Enum):
     BACKWARD = "backward"
 
 
-class StationDTO(TypedDict):
+class Station(BaseModel):
     station_id: int
-    coordinates: Coords
+    lng: float
+    lat: float
     network_id: int
     network_name: str
     fraction: float
 
-    segment_id: NotRequired[int]
-    price_per_liter: NotRequired[float]
-    total_distance_m: NotRequired[float]
-    total_duration_s: NotRequired[float]
-    distance_difference_m: NotRequired[float]
-    duration_difference_s: NotRequired[float]
-    fuel_price: NotRequired[float]
+    segment_id: int | None = None
+    price_per_liter: float | None = None
+    total_distance_m: float | None = None
+    total_duration_s: float | None = None
+    distance_difference_m: float | None = None
+    duration_difference_s: float | None = None
+    fuel_price: float | None = None
+    total_price: float | None = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+    def assign_segment_id(
+            self,
+            route_length_m: float,
+            segment_length_m: float
+    ) -> None:
+        self.segment_id = floor(self.fraction * route_length_m / segment_length_m)
+
+    def add_fuel_price_per_liter(self, price_per_liter: float) -> None:
+        self.price_per_liter = price_per_liter
+
+    def add_total_distance(self, distance_m: float) -> None:
+        self.total_distance_m = distance_m
+
+    def add_total_duration(self, duration_s: float) -> None:
+        self.total_duration_s = duration_s
+
+    def calculate_distance_difference(self, original_distance_m: float) -> None:
+        self.distance_difference_m = self.total_distance_m - original_distance_m
+
+    def calculate_duration_difference(self, original_duration_s: float) -> None:
+        self.duration_difference_s = self.total_duration_s - original_duration_s
+
+    def calculate_fuel_price(self, volume: float) -> None:
+        self.fuel_price = self.price_per_liter * volume
+
+    def calculate_total_price(
+            self, fuel_consumption_1km: float, income_per_minute: float
+    ) -> None:
+        self.total_price = (
+            self.fuel_price
+            + fuel_consumption_1km * self.distance_difference_m / 1000
+            + income_per_minute * self.duration_difference_s / 60
+        )
 
 
 @dataclass
