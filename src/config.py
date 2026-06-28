dd98ee168a3bbed1ba837688893bfaf08435bd21
Oleksandr Skoromnov <oleksandr.skoromnov@renesandro.ai>
2026-06-28 20:01:22 +0300

    [add] test infisical for env

--- src/config.py
+++ src/config.py
@@ -1,9 +1,35 @@
+import os
 from pathlib import Path
 from contextvars import ContextVar
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from infisical_sdk import InfisicalSDKClient
+from dotenv import load_dotenv
+load_dotenv()
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 LOG_ID: ContextVar[str] = ContextVar("LOG_ID", default="")
+
+
+client = InfisicalSDKClient(
+    host="https://app.infisical.com",
+    cache_ttl=None
+)
+
+client.auth.universal_auth.login(
+    client_id=os.getenv("INFISICAL_CLIENT_ID"),
+    client_secret=os.getenv("INFISICAL_CLIENT_SECRET")
+)
+# client.auth.token_auth.login(os.getenv("INFISICAL_TOKEN"))
+
+secrets = client.secrets.list_secrets(
+    project_id=os.getenv("INFISICAL_PROJECT_ID"),
+    environment_slug="dev",
+    secret_path="/"
+)
+
+print(secrets)
+print(type(secrets))
+
 
 
 class Settings(BaseSettings):
@@ -28,4 +54,3 @@ class Settings(BaseSettings):
     )
 
 
-settings = Settings()
